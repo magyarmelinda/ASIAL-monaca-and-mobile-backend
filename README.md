de74398c06d9fe53fcbba2eee6bc77c3832e9200
daydreamheart <m4gy4r.m3l1nd4@gmail.com>
2021-04-07 15:01:23 +0900

    Added some changes.

---
+++ README.md
@@ -0,0 +1,12 @@
+# Monaca and NIFCLOUD Mobile Backend
+
+Created with plain JavaScript.
+
+## Resources
+
+- [First Monaca and Mobile Backend Article](https://qiita.com/natsumo/items/d278c71c0211a7902767#4-mobile-backend-%E3%81%8B%E3%82%89%E3%83%87%E3%83%BC%E3%82%BF%E3%82%92%E5%8F%96%E5%BE%97%E3%81%97%E3%81%A6%E3%81%BF%E3%82%8B)
+- [Monaca Data Registration Repository](https://github.com/NIFCLOUD-mbaas/monaca_data_registration)
+
+## Mobile Backend
+
+- [NIFCLOUD Webpage](https://mbaas.nifcloud.com/en/)
---
+++ www/js/app.js
@@ -0,0 +1,43 @@
+// API key 
+const applicationKey = "**********";
+const clientKey = "**********";
+// SDK initialization 
+const ncmb = new NCMB(applicationKey, clientKey);
+
+// Processing when the button is pressed 
+function pushButton() {
+  // Get the value of the text area 
+  let message = document.getElementById("message").value;
+  alert(message);
+
+  /** Save to mobile backend **/
+  // Generate destination class 
+  const MessageClass = ncmb.DataStore("MessageClass");
+  // Generate class instance 
+  const messageClass = new MessageClass();
+  // Set data Save 
+  messageClass.set("message", message).save();
+}
+
+// Processing when the Omikuji button is pressed 
+function omikuji() {
+  /** Get from mobile backend **/
+  // generate destination class 
+  const Omikuji = ncmb.DataStore("Omikuji");
+  // get processed
+  Omikuji.fetchAll()
+    .then(function (objects) {
+      /* get processing at the time of success */
+      // create a random number // number of data within the 
+      let random = Math.floor(Math.random() * objects.length);
+      // random number th data
+      let object = objects[random];
+      // "result" field value acquisition
+      let result = object.get("result");
+      // displaying the result on the screen 
+      document.getElementById("result").innerText = result;
+    }).catch(function (error) {
+      /* Processing when acquisition fails */
+      alert("Error:" + error.code);
+    })
+}
